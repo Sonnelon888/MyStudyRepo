db09ecc3336f691e5dca576708627c2698b87eb5
Stepan.Mokrov <bearlike00@gmail.com>
2019-06-02 23:40:32 +0300

    fix

--- src/main/java/BinaryParser.java
+++ src/main/java/BinaryParser.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
 
 public class BinaryParser {
-    private Scanner scanner =new Scanner(System.in) ;
-    private String input;
-    private int result;
+    private Scanner scanner = new Scanner(System.in);
+    private double result;
 
-    public void parseBinary() {
+    private Integer parseBinary() {
         System.out.println("Enter binary num: ");
-        input = scanner.nextLine();
-        result = Integer.parseInt(input, 2);
-        System.out.println(result);
+        String input = scanner.nextLine();
+        Double[] values = new Double[input.toCharArray().length];
+        for (int i = 0; i < input.toCharArray().length; i++) {
+            values[i] = Double
+                    .valueOf(
+                            Character
+                                    .toString(
+                                            input
+                                                    .toCharArray()[i]));
+        }
+
+        for (int n = values.length; n > 0; n--) {
+                result += (values[values.length - n] * Math.pow(2, n - 1));
+        }
+        return (int) result;
     }
 
     public static void main(String[] args) {
         BinaryParser parser = new BinaryParser();
-        parser.parseBinary();
+        System.out.println(parser.parseBinary());
     }
 }
