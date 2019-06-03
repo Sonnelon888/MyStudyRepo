983a43cd610fdacd9527736f495c5e86ed2fb2c7
Stepan.Mokrov <bearlike00@gmail.com>
2019-06-03 23:39:13 +0300

    Добавил бинарные операции, получилось избавиться только от одного цикла, так же в условии не сказано о запрете на их использование. Сказано в задании №5

--- src/main/java/BinaryParser.java
+++ src/main/java/BinaryParser.java
@@ -2,25 +2,23 @@ import java.util.Scanner;
 
 public class BinaryParser {
     private Scanner scanner = new Scanner(System.in);
-    private double result;
+
 
     private Integer parseBinary() {
+        int result = 0;
         System.out.println("Enter binary num: ");
         String input = scanner.nextLine();
-        Double[] values = new Double[input.toCharArray().length];
-        for (int i = 0; i < input.toCharArray().length; i++) {
-            values[i] = Double
-                    .valueOf(
-                            Character
-                                    .toString(
-                                            input
-                                                    .toCharArray()[i]));
+        for (int i = input.toCharArray().length; i > 0; i--) {
+            result += Character
+                    .getNumericValue(
+                            input
+                                    .toCharArray()[
+                                    input
+                                            .toCharArray()
+                                            .length - i])
+                    * (1 << (~-i));
         }
-
-        for (int n = values.length; n > 0; n--) {
-                result += (values[values.length - n] * Math.pow(2, n - 1));
-        }
-        return (int) result;
+        return result;
     }
 
     public static void main(String[] args) {
